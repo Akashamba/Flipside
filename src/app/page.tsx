90ec50c7b185664fe7b262430cbd3295181ed209
akashamba <akashambashankar@gmail.com>
2025-06-02 01:59:16 -0400

    moved navbar to components dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 // import { api, HydrateClient } from "@/trpc/server";
 import { SignIn, SignInButton } from "@clerk/nextjs";
-import Navbar from "./components/navbar";
+import Navbar from "../components/navbar";
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
